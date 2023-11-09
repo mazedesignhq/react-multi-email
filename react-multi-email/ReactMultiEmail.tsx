395932a923e673fd8a93cf4ba62d35c3e89caf0b
Eduards Klavins <eduards.klavins@maze.design>
2023-11-09 10:58:41 +0700

    chore(react-multi-email): remove weird behaviour on backspace

--- react-multi-email/ReactMultiEmail.tsx
+++ react-multi-email/ReactMultiEmail.tsx
@@ -202,7 +202,6 @@ export function ReactMultiEmail(props: IReactMultiEmailProps) {
 
       switch (e.key) {
         case 'Enter':
-        case 'Backspace':
           await findEmailAddress(e.currentTarget.value, true);
           break;
         default:
